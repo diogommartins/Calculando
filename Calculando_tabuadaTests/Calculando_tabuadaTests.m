22020c78627f42e6e8b2e58b7626653f9d2f7049
diogomartins <diogo.martins@unirio.br>
2015-06-03 13:39:53 -0300

    Initial Commit

---
+++ Calculando_tabuadaTests/Calculando_tabuadaTests.m
@@ -0,0 +1,40 @@
+//
+//  Calculando_tabuadaTests.m
+//  Calculando_tabuadaTests
+//
+//  Created by Diogo Martins on 6/3/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Calculando_tabuadaTests : XCTestCase
+
+@end
+
+@implementation Calculando_tabuadaTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
