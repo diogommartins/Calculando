4180ee300eb1ab72ead293ebb53a90095242b7ee
diogommartins <diogo.martins@unirio.br>
2015-06-08 14:58:44 -0300

    Adicionada LandingViewController e respectiva scene

---
+++ Calculando_tabuada/LandingViewController.h
@@ -0,0 +1,17 @@
+//
+//  LandingViewController.h
+//  Calculando_tabuada
+//
+//  Created by Diogo Martins on 6/7/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandingViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *btnNewGame;
+
+- (IBAction)touchedStartNewGameButton:(UIButton *)sender;
+
+@end
---
+++ Calculando_tabuada/LandingViewController.m
@@ -0,0 +1,16 @@
+//
+//  LandingViewController.m
+//  Calculando_tabuada
+//
+//  Created by Diogo Martins on 6/7/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#import "LandingViewController.h"
+
+@implementation LandingViewController
+
+- (IBAction)touchedStartNewGameButton:(UIButton *)sender {
+}
+
+@end
